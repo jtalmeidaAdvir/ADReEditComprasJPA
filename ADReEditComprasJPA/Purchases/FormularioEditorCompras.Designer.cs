f230f73e1fd62e1a614cd5db509bf514e83e8e24
jtalmeidaAdvir <jtalmeida@advir.pt>
2025-06-06 09:22:54 +0100

    first commit

---
+++ ADReEditComprasJPA/Purchases/FormularioEditorCompras.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace ADReEditComprasJPA.Purchases
+{
+    partial class FormularioEditorCompras
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.f41 = new PRISDK100.F4();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // f41
+            // 
+            this.f41.AgrupaOutrosTerceiros = false;
+            this.f41.Audit = "mnuTabDocumentosCompra";
+            this.f41.AutoComplete = false;
+            this.f41.BackColorLocked = System.Drawing.SystemColors.ButtonFace;
+            this.f41.CampoChave = "Documento";
+            this.f41.CampoChaveFisica = "";
+            this.f41.CampoDescricao = "Descricao";
+            this.f41.Caption = "Documento:";
+            this.f41.CarregarValoresEdicao = false;
+            this.f41.Categoria = PRISDK100.clsSDKTypes.EnumCategoria.DocumentosCompra;
+            this.f41.ChaveFisica = "";
+            this.f41.ChaveNumerica = false;
+            this.f41.F4Modal = false;
+            this.f41.Font = new System.Drawing.Font("Tahoma", 8.25F);
+            this.f41.IDCategoria = "DocumentosCompra";
+            this.f41.Location = new System.Drawing.Point(3, 12);
+            this.f41.MaxLengthDescricao = 0;
+            this.f41.MaxLengthF4 = 50;
+            this.f41.MinimumSize = new System.Drawing.Size(37, 21);
+            this.f41.Modulo = "CMP";
+            this.f41.MostraDescricao = true;
+            this.f41.MostraLink = true;
+            this.f41.Name = "f41";
+            this.f41.PainesInformacaoRelacionada = false;
+            this.f41.PainesInformacaoRelacionadaMultiplasChaves = false;
+            this.f41.PermiteDrillDown = true;
+            this.f41.PermiteEnabledLink = true;
+            this.f41.PodeEditarDescricao = false;
+            this.f41.ResourceID = 154;
+            this.f41.ResourcePersonalizada = false;
+            this.f41.Restricao = "";
+            this.f41.SelectionFormula = "";
+            this.f41.Size = new System.Drawing.Size(311, 22);
+            this.f41.TabIndex = 0;
+            this.f41.TextoDescricao = "";
+            this.f41.WidthEspacamento = 60;
+            this.f41.WidthF4 = 1590;
+            this.f41.WidthLink = 1575;
+            this.f41.Load += new System.EventHandler(this.f41_Load);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(414, 14);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(65, 20);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(320, 13);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(88, 21);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 54);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(476, 311);
+            this.dataGridView1.TabIndex = 3;
+            // 
+            // FormularioEditorCompras
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.f41);
+            this.Name = "FormularioEditorCompras";
+            this.Size = new System.Drawing.Size(482, 368);
+            this.Text = "FormularioEditorCompras";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.FormularioEditorCompras_FormClosed);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private PRISDK100.F4 f41;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
---
+++ ADReEditComprasJPA/Purchases/FormularioEditorCompras.cs
@@ -0,0 +1,45 @@
+﻿using Primavera.Extensibility.BusinessEntities;
+using Primavera.Extensibility.CustomForm;
+using PrimaveraSDK;
+using PRISDK100;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ADReEditComprasJPA.Purchases
+{
+    public partial class FormularioEditorCompras : CustomForm
+    {
+        private bool controlsInitialized;
+        public FormularioEditorCompras()
+        {
+            InitializeComponent();
+        }
+
+        private void f41_Load(object sender, EventArgs e)
+        {
+            PriSDKContext.Initialize(BSO, PSO);
+            InitializeSDKControls();
+        }
+        private void InitializeSDKControls()
+        {
+            if (!controlsInitialized)
+            {
+                f41.Inicializa(PriSDKContext.SdkContext);
+            }
+        }
+
+        private void FormularioEditorCompras_FormClosed(object sender, System.Windows.Forms.FormClosedEventArgs e)
+        {
+            try
+            {
+                //Ensure that resources released.
+                f41.Termina();
+                controlsInitialized = false;
+            }
+            catch { }
+        }
+    }
+}
